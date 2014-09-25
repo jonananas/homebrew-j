f4531589759705769c97276e48d24feb569cfc7b
Jonas Andersson <jonananas@gmail.com>
2014-09-25 16:36:48 +0200

    updated sha1

--- jboss-deploy-latest.rb
+++ jboss-deploy-latest.rb
@@ -3,7 +3,7 @@ require "formula"
 class JbossDeployLatest < Formula
   homepage "https://github.com/jonananas/jboss-deploy-latest"
   url "https://github.com/jonananas/jboss-deploy-latest/tree/0.0.1"
-  sha1 "7528a9e491b15334f17ab12efd3d5e5fa2b8cdd5"
+  sha1 "6702ae14b4e16bfca7fc4698001b5265412a1454"
 
   def install
     bin.install 'deploy.sh'
