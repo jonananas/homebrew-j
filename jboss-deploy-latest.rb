e6f769bce8ad142482fadb3d302d18e2ec324bb9
Jonas Andersson <jonananas@gmail.com>
2014-09-25 16:33:58 +0200

    backed

---
+++ jboss-deploy-latest.rb
@@ -0,0 +1,11 @@
+require "formula"
+
+class JbossDeployLatest < Formula
+  homepage "https://github.com/jonananas/jboss-deploy-latest"
+  url "https://github.com/jonananas/jboss-deploy-latest/tree/0.0.1"
+  sha1 "7528a9e491b15334f17ab12efd3d5e5fa2b8cdd5"
+
+  def install
+    bin.install 'deploy.sh'
+  end
+end
