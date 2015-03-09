2620771997199081b591915ca7d825acddea950c
Jonas Andersson <jonananas@gmail.com>
2015-03-09 13:02:32 +0100

    Updated to deploy.sh 0.0.6

--- jboss-deploy-latest.rb
+++ jboss-deploy-latest.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class JbossDeployLatest < Formula
   homepage "https://github.com/jonananas/jboss-deploy-latest"
-  url "https://raw.githubusercontent.com/jonananas/jboss-deploy-latest/0.0.5/deploy.sh"
-  sha1 "0d0be536c8a8551a68e3532bbd5016fb9adb90bb"
+  url "https://raw.githubusercontent.com/jonananas/jboss-deploy-latest/0.0.6/deploy.sh"
+  sha1 "56ad3bc8672cfb0e2a068f7766052861552fdf02"
 
   def install
     bin.install 'deploy.sh'
