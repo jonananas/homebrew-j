3d05476f0336f7feac2fad5d4f0d9a5fbaa8ff49
Jonas Andersson <jonananas@gmail.com>
2015-05-22 10:13:56 +0200

    Backed to deploy 0.0.9

--- jboss-deploy-latest.rb
+++ jboss-deploy-latest.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class JbossDeployLatest < Formula
   homepage "https://github.com/jonananas/jboss-deploy-latest"
-  url "https://raw.githubusercontent.com/jonananas/jboss-deploy-latest/0.0.10/deploy.sh"
+  url "https://raw.githubusercontent.com/jonananas/jboss-deploy-latest/0.0.9/deploy.sh"
   sha1 "24f648f0d664a14b4d73e03b50a492279989cc0e"
 
   def install
